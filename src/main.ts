f82f8de348d917834f930f07bc33ab830c295960
JoramirJr <joramirjunior@gmail.com>
2024-06-18 16:00:47 -0300

    railway app required prod changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import * as dotenv from 'dotenv';
+
+const port = process.env.PORT || 3001;
 
 async function bootstrap() {
-  dotenv.config();
   
   const app = await NestFactory.create(AppModule);
 
@@ -14,6 +14,6 @@ async function bootstrap() {
   };
   app.enableCors(corsOptions);
 
-  await app.listen(3001);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
