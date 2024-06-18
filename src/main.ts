55317d54a33b086b0a77c08b21f8281652c77431
JoramirJr <joramirjunior@gmail.com>
2024-06-18 15:48:40 -0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
