4e4655e50a1d094bab6e58fe9f6f061e54fbefb2
JoramirJr <joramirjunior@gmail.com>
2024-06-18 19:42:58 -0300

    final tweaks for prod

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://main--clepher-dashboard.netlify.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   };
   app.enableCors(corsOptions);
